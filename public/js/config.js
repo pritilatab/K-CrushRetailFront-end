2e2a68fea907d9f58a13cc84558ea627017610c0
sayantanpc <59252606+sayantanpc@users.noreply.github.com>
2020-01-22 16:18:23 +0530

    init

---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+//global config
+
+_globalVar = {
+    "backEndAppDomain": "kcrushretailbackend-walmart-team-a.gamification-d3c0cb24e2b77f6869027abe3de4bca3-0001.sng01.containers.appdomain.cloud",
+    "prodPage": 1, //page no.
+    "perPage": 12, //limit 
+    "defaultCategory": "all", //category
+    "prodURLbase": "/products/", //URL path for products
+    "searchURLbase": "/searchv2/", //URL path for search
+    "pBarBaseClass": "progress-bar" //base css class name of progress bar
+  };
